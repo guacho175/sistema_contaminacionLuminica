95f402279cb2294670065ccbd9317266cf55d84b
Arnoldo <arnold.aguirre91@gmail.com>
2024-10-12 17:53:15 -0300

    Integración mod del  nivel de cumplimiento

--- mapa/views.py
+++ mapa/views.py
@@ -1,36 +1,52 @@
-from django.shortcuts import render
-from .models import Location 
+from django.shortcuts import render, redirect
+from .models import Location
 import folium
 from folium.plugins import MarkerCluster
 
 def mapa(request):
-    # Recupero todas los sensores
-    locations = Location.objects.all()
+    # Recupero todas las ubicaciones
+    sensors = Location.objects.all()
 
-    # Defino el mapa
+    # Si es una solicitud POST, actualizo el cumplimiento del sensor seleccionado
+    if request.method == 'POST':
+        sensor_id = request.POST.get('sensor_id')
+        new_cumpl = request.POST.get('cumpl')
+
+        # Actualiza el cumplimiento del sensor seleccionado
+        sensor = Location.objects.get(id=sensor_id)
+        sensor.cumpl = new_cumpl
+        sensor.save()
+
+        # Redirige a la misma vista para actualizar el mapa
+        return redirect('mapa')
+
+    # Genero el mapa
     initialMap = folium.Map(location=[-29.9055324, -71.2313308], zoom_start=10)
-
-    # Creamos el objeto de Clustering para agrupar los marcadores
     marker_cluster = MarkerCluster().add_to(initialMap)
 
-    # Itera sobre las ubicaciones y añade cada marcador con su icono al cluster
-    for sensor in locations:
+    # Itera sobre los sensores y añade un marcador para cada uno
+    for sensor in sensors:
         # Determina el icono basado en el valor de cumplimiento
         if sensor.cumpl == 0:
             icon = folium.Icon(icon="minus-sign", prefix="glyphicon", color="gray")
         elif sensor.cumpl == 1:
             icon = folium.Icon(icon="remove-sign", prefix="glyphicon", color="red")
+        elif sensor.cumpl == 2:
+            icon = folium.Icon(icon="ok-sign", prefix="glyphicon", color="orange")
         else:
             icon = folium.Icon(icon="ok-sign", prefix="glyphicon", color="green")
 
-        # Añade el marcador al cluster
+        # Añade el marcador al clúster
         folium.Marker(
             location=[sensor.lat, sensor.lng],
-            popup=sensor.name,  # Muestra el nombre del sensor al hacer clic
-            tooltip=sensor.address,  # Muestra la dirección cuando se pasa el cursor
-            icon=icon  # Usa el icono definido anteriormente 
+            popup=sensor.name,
+            tooltip=sensor.address,
+            icon=icon
         ).add_to(marker_cluster)
 
-    # Renderiza el mapa con los marcadores en el template
-    context = {'map': initialMap._repr_html_()}
+    # Renderiza el mapa y el formulario en el template
+    context = {
+        'map': initialMap._repr_html_(),
+        'sensors': sensors  # Envío los sensores al template para mostrarlos en el formulario
+    }
     return render(request, 'mapa/mapa_mediciones.html', context)
