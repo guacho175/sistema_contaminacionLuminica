1490c65774cd00a5756ffda5d1f4e6eca8211696
Arnoldo <arnold.aguirre91@gmail.com>
2024-10-12 14:56:17 -0300

    Inclusión de cluter al mapa

--- mapa/views.py
+++ mapa/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render
 from .models import Location 
 import folium
+from folium.plugins import MarkerCluster
 
-# Create your views here.
 def mapa(request):
+    # Recupero todas los sensores
+    locations = Location.objects.all()
+
+    # Defino el mapa
     initialMap = folium.Map(location=[-29.9055324, -71.2313308], zoom_start=10)
-    
-    # Itera sobre las ubicaciones guardadas en la base de datos y añade un marcador por cada una
-    sensors = Location.objects.all()  # Obtiene todos los sensores
-    for sensor in sensors:
+
+    # Creamos el objeto de Clustering para agrupar los marcadores
+    marker_cluster = MarkerCluster().add_to(initialMap)
+
+    # Itera sobre las ubicaciones y añade cada marcador con su icono al cluster
+    for sensor in locations:
         # Determina el icono basado en el valor de cumplimiento
         if sensor.cumpl == 0:
             icon = folium.Icon(icon="minus-sign", prefix="glyphicon", color="gray")
@@ -16,14 +22,15 @@ def mapa(request):
             icon = folium.Icon(icon="remove-sign", prefix="glyphicon", color="red")
         else:
             icon = folium.Icon(icon="ok-sign", prefix="glyphicon", color="green")
-        
+
+        # Añade el marcador al cluster
         folium.Marker(
             location=[sensor.lat, sensor.lng],
             popup=sensor.name,  # Muestra el nombre del sensor al hacer clic
             tooltip=sensor.address,  # Muestra la dirección cuando se pasa el cursor
             icon=icon  # Usa el icono definido anteriormente 
-        ).add_to(initialMap)
+        ).add_to(marker_cluster)
 
     # Renderiza el mapa con los marcadores en el template
-    context = {'map':initialMap._repr_html_()}
+    context = {'map': initialMap._repr_html_()}
     return render(request, 'mapa/mapa_mediciones.html', context)
