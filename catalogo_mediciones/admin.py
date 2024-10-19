2117a0c62b8bbff3b3e38d1cf904882f1f13a23a
Arnoldo <arnold.aguirre91@gmail.com>
2024-10-19 18:23:07 -0300

    Modificacion al Modelo art5, creacion de tablas usuario y rol

--- catalogo_mediciones/admin.py
+++ catalogo_mediciones/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import tipo_alumbrado_art5
+from .models import Tipo_alumbrado_art5, Usuario, Rol
 
 # Register your models here.
-admin.site.register(tipo_alumbrado_art5)
+admin.site.register(Tipo_alumbrado_art5)
+admin.site.register(Usuario)
+admin.site.register(Rol)
