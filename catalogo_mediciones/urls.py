377454ca4474d19111edd469d09c54aa3cd71dc9
Arnoldo <arnold.aguirre91@gmail.com>
2024-10-20 22:28:18 -0300

    Implementacion del catalogo de mediciones, detalle de nediciones y editar medicion

--- catalogo_mediciones/urls.py
+++ catalogo_mediciones/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from . import views as v
+from .views import MedicionUpdateView
+
 
 urlpatterns = [
 
     path('', v.catalogo_mediciones, name='catalogo'),
+    path('catalogo/detalle/<int:id>/', v.detalle_medicion, name='detalle_medicion'),
+    path('medicion/<int:pk>/editar/', MedicionUpdateView.as_view(), name='editar_medicion'),
+
+
 
 ]
--- catalogo_mediciones/views.py
+++ catalogo_mediciones/views.py
@@ -1,6 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse_lazy
+from .models import Tipo_alumbrado_art5
+from django.views.generic.edit import UpdateView
 
-# Create your views here.
+
 def catalogo_mediciones(request):
-    context = {}
+    mediciones_art5 = Tipo_alumbrado_art5.objects.all()  # Obtenemos todas las mediciones
+    context = {
+        'mediciones_art5': mediciones_art5
+    }
     return render(request, 'catalogo_mediciones/catalogo_mediciones.html', context)
+
+def detalle_medicion(request, id):
+    medicion = get_object_or_404(Tipo_alumbrado_art5, id=id)  # Obtener la medición
+    context = {
+        'medicion': medicion
+    }
+    return render(request, 'catalogo_mediciones/detalle_medicion.html', context)
+
+class MedicionUpdateView(UpdateView):
+    model = Tipo_alumbrado_art5
+    fields = [
+        'direccion', 'latitud', 'longitud', 'tipo', 'menor_igual_90grados', 
+        'mayor_90grados', 'clase_luminaria', 'emision_reflexion', 
+        'proteccion_especial', 'radiancia_espectral', 'emision_conjunta', 
+        'usuario','nivel_cumplimiento','observaciones'
+    ]
+    template_name = 'catalogo_mediciones/editar_medicion.html'
+    success_url = reverse_lazy('catalogo')
