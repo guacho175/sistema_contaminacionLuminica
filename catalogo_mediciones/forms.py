a9e0a802ad4d195c1098ad09c976379ca9d4e758
Arnoldo <arnold.aguirre91@gmail.com>
2024-10-22 16:07:14 -0300

    Implementacion CRUD catalogo_mediciones

--- catalogo_mediciones/forms.py
+++ catalogo_mediciones/forms.py
@@ -1,15 +1,51 @@
 from django import forms
-from .choices import tipos_alumbrado, nivel_cumplimiento
-from .models import Rol, Usuario, Tipo_alumbrado_art5
+from .choices import tipos_alumbrado
+from .models import Tipo_alumbrado_art5, Tipo_alumbrado_art6, CatalogoMedicion
+
+
+class MedicionForm(forms.ModelForm):
+
+    medicion_art5 = forms.ModelChoiceField(
+        queryset=Tipo_alumbrado_art5.objects.filter(estado=0),
+        required=False,
+        empty_label="Selecciona una medición de tipo art5",
+        widget=forms.Select(attrs={'class': 'form-control'})
+    )
+
+    medicion_art6 = forms.ModelChoiceField(
+    queryset=Tipo_alumbrado_art6.objects.filter(estado=0),
+    required=False,
+    empty_label="Selecciona una medición de tipo art6",
+    widget=forms.Select(attrs={'class': 'form-control'})
+    )   
+
+    class Meta:
+        model = CatalogoMedicion
+        fields = '__all__'
+    
+    def clean(self):
+        cleaned_data = super().clean()
+        medicion_art5 = cleaned_data.get("medicion_art5")
+        medicion_art6 = cleaned_data.get("medicion_art6")
+
+        # Verificar que solo uno de los dos campos esté completado
+        if medicion_art5 and medicion_art6:
+            raise forms.ValidationError("Solo puedes seleccionar una medición: Art. 5 o Art. 6, no ambas.")
+        
+        if not medicion_art5 and not medicion_art6:
+            raise forms.ValidationError("Debes seleccionar una medición: Art. 5 o Art. 6.")
+        
+        return cleaned_data
+
 
 class EditarMedicion(forms.ModelForm):
     class Meta:
         model = Tipo_alumbrado_art5
         fields = [
-            'direccion', 'latitud', 'longitud', 'tipo', 'menor_igual_90grados',
+            'direccion', 'latitud', 'longitud', 'tipo_alumbrado', 'menor_igual_90grados',
             'mayor_90grados', 'clase_luminaria', 'emision_reflexion',
             'proteccion_especial', 'radiancia_espectral', 'emision_conjunta',
-            'usuario', 'nivel_cumplimiento', 'observaciones'
+            'usuario', 'observaciones','estado'
         ]
         
         # Definir etiquetas para los campos del formulario
@@ -17,7 +53,7 @@ class EditarMedicion(forms.ModelForm):
             'direccion': 'Ubicación',
             'latitud': 'Latitud',
             'longitud': 'Longitud',
-            'tipo': 'Tipo de Alumbrado',
+            'tipo_alumbrado': 'Tipo de Alumbrado',
             'menor_igual_90grados': 'Intensidad Luminosa <= 90º',
             'mayor_90grados': 'Intensidad Luminosa > 90º',
             'clase_luminaria': 'Clase de Luminaria',
@@ -27,7 +63,7 @@ class EditarMedicion(forms.ModelForm):
             'emision_conjunta': 'Emisión Conjunta Considerada',
             'usuario': 'Inspector',
             'observaciones': 'Observaciones',
-            'nivel_cumplimiento': 'Nivel de Cumplimiento'
+            'estado': 'Estado de medición'
         }
         
         # Widgets para personalizar la representación de los campos en el formulario
@@ -35,7 +71,7 @@ class EditarMedicion(forms.ModelForm):
             'direccion': forms.TextInput(attrs={'class': 'form-control'}),
             'latitud': forms.NumberInput(attrs={'class': 'form-control'}),
             'longitud': forms.NumberInput(attrs={'class': 'form-control'}),
-            'tipo': forms.Select(choices=tipos_alumbrado, attrs={'class': 'form-control'}),
+            'tipo_alumbrado': forms.Select(choices=tipos_alumbrado, attrs={'class': 'form-control'}),
             'menor_igual_90grados': forms.NumberInput(attrs={'class': 'form-control'}),
             'mayor_90grados': forms.NumberInput(attrs={'class': 'form-control'}),
             'clase_luminaria': forms.TextInput(attrs={'class': 'form-control'}),
@@ -45,6 +81,6 @@ class EditarMedicion(forms.ModelForm):
             'emision_conjunta': forms.Select(choices=[('SI', 'Sí'), ('NO', 'No')], attrs={'class': 'form-control'}),
             'usuario': forms.Select(attrs={'class': 'form-control'}),
             'observaciones': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
-            'nivel_cumplimiento': forms.Select(choices=nivel_cumplimiento, attrs={'class': 'form-control'})
+            'estado': forms.Select(choices=[('1', 'En catálogo'), ('0', 'Fuera del catálogo')], attrs={'class': 'form-control'})
         }
 
--- catalogo_mediciones/urls.py
+++ catalogo_mediciones/urls.py
@@ -3,8 +3,10 @@ from . import views as v
 
 
 urlpatterns = [
-
     path('', v.catalogo_mediciones, name='catalogo'),
     path('catalogo/detalle/<int:id>/', v.detalle_medicion, name='detalle_medicion'),
+    path('registrar_medicion/', v.registrar_medicion, name='registrar_medicion'),
+    path('medicion/<int:id>/eliminar/', v.eliminar_medicion, name='eliminar_medicion'),
     path('medicion/<int:id>/editar/', v.editar_medicion, name='editar_medicion'),
+
 ]
