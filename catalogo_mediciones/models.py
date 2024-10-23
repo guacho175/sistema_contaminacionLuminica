fedfb17e330ec40e178b85cc84f9477afaa2fca9
Arnoldo <arnold.aguirre91@gmail.com>
2024-10-23 04:14:07 -0300

    Implementacion de CRUD, falta Update

--- catalogo_mediciones/models.py
+++ catalogo_mediciones/models.py
@@ -37,9 +37,10 @@ class Usuario(models.Model):
         ordering = ['nombre', 'paterno', 'materno']
 
 
-class Tipo_alumbrado_art5(models.Model):
+class TipoAlumbradoArt5(models.Model):
+    nivel_cumplimiento = models.CharField(max_length=1, null=True, blank=True, choices=nivel_cumplimiento, default='0')
+    tipo = models.CharField(max_length=4,choices=tipos_alumbrado, default='art5')
     estado = models.IntegerField(default='0')
-    tipo_alumbrado = models.CharField(max_length=4,choices=tipos_alumbrado, default='art5')
     direccion = models.CharField(max_length=250, verbose_name='Ubicación')
     latitud = models.FloatField(verbose_name='Latitud')
     longitud = models.FloatField(verbose_name='Longitud')
@@ -51,47 +52,47 @@ class Tipo_alumbrado_art5(models.Model):
     radiancia_espectral = models.FloatField(verbose_name='Radiancia Espectral')
     emision_conjunta = models.CharField(max_length=2, verbose_name='Emision conjunta considerada')
     observaciones = models.CharField(max_length=500, blank=True, null=True, verbose_name='Observaciones')
-    usuario = models.ForeignKey(Usuario, null=True, on_delete=models.RESTRICT)
+    usuario = models.ForeignKey(Usuario,null=True, on_delete=models.SET_NULL)
     creado = models.DateTimeField(default=timezone.now)
 
     class Meta:
-        verbose_name = 'Medicion_art5'
-        verbose_name_plural = 'Mediciones_art5'
+        verbose_name = 'Medicion art5'
+        verbose_name_plural = 'Mediciones art5'
 
     def __str__(self):
         return self.direccion
 
 
-class Tipo_alumbrado_art6(models.Model):
+class TipoAlumbradoArt6(models.Model):
+    nivel_cumplimiento = models.CharField(max_length=1, null=True, blank=True, choices=nivel_cumplimiento, default='0')
+    tipo = models.CharField(max_length=4,choices=tipos_alumbrado, default='art6')
     estado = models.IntegerField(default='0')
-    tipo_alumbrado = models.CharField(max_length=4,choices=tipos_alumbrado, default='art6')
     direccion = models.CharField(max_length=250, verbose_name='Ubicación')
     latitud = models.FloatField(verbose_name='Latitud')
     longitud = models.FloatField(verbose_name='Longitud')
     tipo_flujo_luminoso = models.CharField(max_length=1, choices=tipo_flujo_luminoso, default='', verbose_name='Tipo de flujo luminoso')
     valor_flujo_luminoso = models.FloatField(verbose_name='Flujo luminoso de luminaria')
-    Hemisferio_superior = models.FloatField(verbose_name='Emision al hemisferio superior')
+    Hemisferio_superior = models.FloatField(verbose_name='Emisión al hemisferio superior')
     proteccion_especial = models.CharField(max_length=2, verbose_name='Área de protección especial')
-    reflexion_luminancia = models.FloatField(verbose_name='Emisiòn por Reflexiòn: Luminancia')
+    reflexion_luminancia = models.FloatField(verbose_name='Emisión por Reflexión: Luminancia')
     temperatura_luz_blanca = models.FloatField(verbose_name='Temperatura de color: Luz Blanca')
     temperatura_luz_color = models.FloatField(verbose_name='Temperatura de color: Luces multicolor')
     limite_horario = models.CharField(max_length=2, verbose_name='Limite horario')
     horario_extendido = models.CharField(max_length=2, verbose_name='Permiso de extención horaria')
     observaciones = models.CharField(max_length=500, blank=True, null=True, verbose_name='Observaciones')
-    usuario = models.ForeignKey(Usuario, null=True, on_delete=models.RESTRICT)
+    usuario = models.ForeignKey(Usuario, null=True, on_delete=models.SET_NULL) # el objeto hijo existe incluso si el objeto padre es eliminado
     creado = models.DateTimeField(default=timezone.now)
 
     class Meta:
-        verbose_name = 'Medicion_art6'
+        verbose_name = 'Medicion art6'
         verbose_name_plural = 'Mediciones art6'
 
     def __str__(self):
         return self.direccion
 
 class CatalogoMedicion(models.Model):
-    nivel_cumplimiento = models.CharField(max_length=1, null=True, blank=True, choices=nivel_cumplimiento, default='0')
-    medicion_art5 = models.ForeignKey(Tipo_alumbrado_art5, null=True, blank=True, on_delete=models.SET_NULL)
-    medicion_art6 = models.ForeignKey(Tipo_alumbrado_art6, null=True, blank=True, on_delete=models.SET_NULL)
+    medicion_art5 = models.ForeignKey(TipoAlumbradoArt5, null=True, blank=True, on_delete=models.RESTRICT)
+    medicion_art6 = models.ForeignKey(TipoAlumbradoArt6, null=True, blank=True, on_delete=models.RESTRICT)
     
     class Meta:
         verbose_name = 'Catálogo de Mediciones'
@@ -99,7 +100,7 @@ class CatalogoMedicion(models.Model):
 
     def __str__(self):
         if self.medicion_art5:
-            return f"Medición Art. 5 - {self.medicion_art5.direccion}"
+            return f"Medición Art. 5 - {self.medicion_art5.tipo}"
         elif self.medicion_art6:
-            return f"Medición Art. 6 - {self.medicion_art6.direccion}"
+            return f"Medición Art. 6 - {self.medicion_art6.tipo}"
         return "Medición sin detalles"
--- catalogo_mediciones/urls.py
+++ catalogo_mediciones/urls.py
@@ -7,6 +7,4 @@ urlpatterns = [
     path('catalogo/detalle/<int:id>/', v.detalle_medicion, name='detalle_medicion'),
     path('registrar_medicion/', v.registrar_medicion, name='registrar_medicion'),
     path('medicion/<int:id>/eliminar/', v.eliminar_medicion, name='eliminar_medicion'),
-    path('medicion/<int:id>/editar/', v.editar_medicion, name='editar_medicion'),
-
 ]
