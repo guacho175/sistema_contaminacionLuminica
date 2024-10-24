00132b9ad69f6c8769de321a8274bdb9edbce982
Arnoldo <arnold.aguirre91@gmail.com>
2024-10-24 17:05:35 -0300

    Implementacion de mapa para visualizar mediciones del catálogo

--- catalogo_mediciones/views.py
+++ catalogo_mediciones/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .forms import MedicionForm
 from .models import TipoAlumbradoArt5, TipoAlumbradoArt6, CatalogoMedicion
+import folium
+from folium.plugins import MarkerCluster
 
 
 def catalogo_mediciones(request):
@@ -8,10 +10,52 @@ def catalogo_mediciones(request):
     mediciones_art5 = TipoAlumbradoArt5.objects.all()
     mediciones_art6 = TipoAlumbradoArt6.objects.all()
 
+    # Crear el mapa
+    initialMap = folium.Map(location=[-29.9055324, -71.2313308], zoom_start=11) #, tiles='CartoDB dark_matter'
+    marker_cluster = MarkerCluster().add_to(initialMap)
+
+    # Iterar sobre las mediciones del catálogo
+    for medicion in catalogo_mediciones:
+        # Condicional para las mediciones de art5
+        if medicion.medicion_art5:
+            lat = medicion.medicion_art5.latitud
+            lng = medicion.medicion_art5.longitud
+            direccion = medicion.medicion_art5.direccion
+            nivel_cumpl = medicion.medicion_art5.nivel_cumplimiento
+            tipo = "Art 5"
+            usuario = medicion.medicion_art5.usuario
+        # Condicional para las mediciones de art6
+        elif medicion.medicion_art6:
+            lat = medicion.medicion_art6.latitud
+            lng = medicion.medicion_art6.longitud
+            direccion = medicion.medicion_art6.direccion
+            nivel_cumpl = medicion.medicion_art6.nivel_cumplimiento
+            tipo = "Art 6"
+            usuario = medicion.medicion_art6.usuario
+
+        # Determinar el icono según el nivel de cumplimiento
+        if nivel_cumpl == '0':
+            icon = folium.Icon(icon="minus-sign", prefix="glyphicon", color="gray")
+        elif nivel_cumpl == '1':
+            icon = folium.Icon(icon="remove-sign", prefix="glyphicon", color="red")
+        elif nivel_cumpl == '2':
+            icon = folium.Icon(icon="ok-sign", prefix="glyphicon", color="orange")
+        else:
+            icon = folium.Icon(icon="ok-sign", prefix="glyphicon", color="green")
+
+        # Agregar el marcador al cluster
+        folium.Marker(
+            location=[lat, lng],
+            popup=f'{direccion} ({tipo}) - Inspector: {usuario}',
+            tooltip=f'{direccion} - Nivel de Cumplimiento: {nivel_cumpl}',
+            icon=icon
+        ).add_to(marker_cluster)
+
     context = {
         'catalogo_mediciones': catalogo_mediciones,
         'mediciones_art5': mediciones_art5,
         'mediciones_art6': mediciones_art6,
+        'map': initialMap._repr_html_()  # El mapa se pasa como HTML
     }
 
     return render(request, 'catalogo_mediciones/catalogo_mediciones.html', context)
