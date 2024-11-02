04a24725722454d7f7d1d85deeb17842dd6cf868
Arnoldo <arnold.aguirre91@gmail.com>
2024-11-02 17:30:09 -0300

    implementacionlistado de catalogo mediciones

--- catalogo_mediciones/admin.py
+++ catalogo_mediciones/admin.py
@@ -1,2 +1,20 @@
 from django.contrib import admin
+from .models import InstrumentoMedicion, DetalleMedicion, Medicion
+
+# Register your models here.
+class InstrumentoMedicionAdmin(admin.ModelAdmin):
+    list_display = ['id', 'tipo', 'marca', 'modelo', 'num_serie']
+
+
+class DetalleMedicionAdmin(admin.ModelAdmin):
+    list_display = ['id', 'longitud', 'latitud', 'temperatura',
+                    'humedad', 'valor_medido', 'instrumento_medicion']
+
+class MedicionAdmin(admin.ModelAdmin):
+    list_display = ['id', 'cumplimiento', 'inspector', 'proyecto',
+                    'detalleMedicion']
+
+admin.site.register(InstrumentoMedicion, InstrumentoMedicionAdmin)
+admin.site.register(DetalleMedicion, DetalleMedicionAdmin)
+admin.site.register(Medicion, MedicionAdmin)
 
--- catalogo_mediciones/models.py
+++ catalogo_mediciones/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 from django.utils import timezone
+from .choices import cumplimiento, tipo_instrumento
+from proyectos.models import Proyecto
+from inspectores.models import Inspector
 
+class InstrumentoMedicion(models.Model):
+    tipo = models.CharField(max_length=1, choices=tipo_instrumento, verbose_name='Iipo')
+    marca = models.CharField(max_length=50, verbose_name='Marca')
+    modelo = models.CharField(max_length=50, verbose_name='Modelo')
+    num_serie = models.CharField(max_length=50, verbose_name='N° de Serie')
+    
+    def __str__(self):
+        return "{} {}".format(self.tipo, self.num_serie)
+        
+    class Meta:
+        db_table = 'instrumento_medicion'
+        verbose_name = 'Instrumento de medición'
+        verbose_name_plural = 'Instrumentos de medición'
+
+
+class DetalleMedicion(models.Model):
+    longitud = models.FloatField(verbose_name='Longitud')
+    latitud = models.FloatField(verbose_name='Latitud')
+    temperatura = models.FloatField(verbose_name='Temperatura (°C)')
+    humedad = models.FloatField(verbose_name='Humedad (%)')
+    valor_medido = models.FloatField(verbose_name='Valor medido')
+    instrumento_medicion = models.ForeignKey(InstrumentoMedicion, null=False, on_delete=models.RESTRICT)
+    creado = models.DateTimeField(default=timezone.now, editable=False)    
+
+
+class Medicion(models.Model):
+    cumplimiento = models.CharField(max_length=1, choices=cumplimiento, blank=True, null=True, verbose_name='Cumplimiento')
+    inspector = models.ForeignKey(Inspector, null=False, on_delete=models.RESTRICT)
+    proyecto = models.ForeignKey(Proyecto, null=False, on_delete=models.RESTRICT)
+    detalleMedicion = models.ForeignKey(DetalleMedicion, null=True, blank=True, on_delete=models.RESTRICT)
